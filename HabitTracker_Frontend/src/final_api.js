85395c148beb1870ec495f933cb9dc271a2c7fc3
StanleyChen6 <stanley196c@gmail.com>
2026-06-07 10:36:15 -0400

    bug fixes and removed unused autogenerated expo files

--- HabitTracker_Frontend/src/final_api.js
+++ HabitTracker_Frontend/src/final_api.js
@@ -6,11 +6,11 @@ This will act as an intemediary helping us easily map API calls to/from backend 
 our React frontend.
 
 We do need to create a few more api calls:
-1) findUser that searches for a user in database given a query
+1) searchUsers that searches for a user in database given a query
 2) getRecentActivity fetches most recent logs of all accepted Friends that occured at most __ hours ago
 */
 
-const API_URL = 'http://localhost:5000'; 
+const API_URL = 'http://192.168.1.42:8080';
 
 // Create an Axios instance
 // This helps us create async network request with backend
@@ -30,8 +30,8 @@ api.interceptors.request.use(async (config) => {
 // LOGIN/REGISTER API
 export const authAPI = {
   login: (credentials) => api.post('/login', credentials),
-  logout: () => api.post('/logout'),
-  createUser: (userData) => api.post('/users', userData),
+  logout: () => SecureStore.deleteItemAsync('userToken'),
+  register: (userData) => api.post('/register', userData), 
 };
 
 // USER API
@@ -39,6 +39,7 @@ export const userAPI = {
   updateMe: (data) => api.put('/users/me', data),
   deleteMe: () => api.delete('/users/me'),
   deleteUser: (id) => api.delete(`/users/${id}`), // Admin only
+  searchUsers: (query) => api.get(`/users/search?query=${query}`),
 };
 
 // HABIT API
@@ -55,19 +56,21 @@ export const habitAPI = {
 // HABITLOG API
 export const logAPI = {
   createLog: (habitId, data) => api.post(`/habits/${habitId}/log`, data),
-  deleteLog: (logId) => api.delete(`/habitsLogs/${logId}`),
-  updateLog: (logId, data) => api.put(`/habitsLogs/${logId}`, data),
+  deleteLog: (habitId, logId) => api.delete(`/habits/${habitId}/logs/${logId}`),
+  updateLog: (habitId, logId, data) => api.put(`/habits/${habitId}/logs/${logId}`, data),
   getLogs: (habitId) => api.get(`/habits/${habitId}/logs`),
 };
 
 // FRIEND API
 export const friendAPI = {
-  sendRequest: (userId) => api.post(`/friendRequest/${userId}`),
-  acceptRequest: (requestId) => api.put(`/friendRequest/${requestId}`),
-  deleteRequest: (requestId) => api.delete(`/friendRequest/${requestId}`),
-  getRequests: () => api.get('/friendRequests'),
+  sendRequest: (userId) => api.post('/friend_requests', { RecipientID: userId }),
+  acceptRequest: (requestId) => api.put(`/friend_requests/${requestId}`),
+  deleteRequest: (requestId) => api.delete(`/friend_requests/${requestId}`),
+  getRequests: () => api.get('/friend_requests/incoming'),
   getFriends: () => api.get('/friends'),
   unfriend: (friendId) => api.delete(`/friends/${friendId}`),
+  getRecentActivity: () => api.get('/friends/activity'),
+  getFriendProfile: (friendId) => api.get(`/friends/${friendId}/profile`), 
 };
 
 export default api;
